b9cc6e3c58fc34080371c5fb6f79e4039b6cc02e
jaehe0130 <165889783+jaehe0130@users.noreply.github.com>
2025-11-24 10:38:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 import streamlit as st
-import time
 
-st.set_page_config(
-    page_title="MoodFit",
-    page_icon="🏋️",
-    layout="centered"
-)
+st.set_page_config(page_title="MoodFit", page_icon="🏋️", layout="centered")
 
-# ----------------------------
-# 이미지 + 텍스트 UI
-# ----------------------------
 st.markdown("<div style='height:12vh;'></div>", unsafe_allow_html=True)
 
 col1, col2, col3 = st.columns([1, 2, 1])
@@ -20,19 +12,14 @@ st.markdown("""
 <h1 style="text-align:center; font-size:42px; font-weight:900; margin-top:15px;">
 🏋️ MoodFit
 </h1>
-
-<p style='text-align:center; font-size:18px; color:#333; margin-top:25px;'>
-오늘의 감정을 선택하면<br>
-당신에게 딱 맞는 운동 루틴을 추천해드릴게요!
-</p>
 """, unsafe_allow_html=True)
 
-# ----------------------------
-# Auto redirect logic
-# ----------------------------
-if "redirected" not in st.session_state:
-    st.session_state.redirected = True
-    time.sleep(2)
-    st.rerun()   # <<<<<<<< HERE
-else:
-    st.switch_page("1_user_info2")
+# ---- JS Redirect (2초 뒤 이동)
+st.markdown("""
+<script>
+setTimeout(function() {
+    window.location.href = "/1_user_info2";
+}, 2000);
+</script>
+""", unsafe_allow_html=True)
+
